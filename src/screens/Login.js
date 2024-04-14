25d91618397893422974c860d32a3648609184c8
Maria Gabriella Victor <109552474+gabriellavsx@users.noreply.github.com>
2024-04-14 20:43:39 -0300

    login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,24 +1,27 @@
-import { View } from 'react-native'
-import { Button } from 'react-native-paper'
+import { View, Text, StyleSheet, Image} from 'react-native'
+import { Button, TextInput } from 'react-native-paper'
 
 const Login = (props) => {
 
   const goToAgradecimentos = () => {
-
     props.navigation.navigate('Agradecimentos')
-
   }
-
-  const goToModificarPesquisa = () => {
-      
+  const goToModificarPesquisa = () => {    
       props.navigation.navigate('ModificarPesquisa')
-  
-    }
-
+  }
+  const goToHome = () => {    
+    props.navigation.navigate('Home')
+  }
   return (
 
-    <View>
-      
+    <View style={styles.body}>
+      <Text style={styles.text}>E-mail</Text>
+      <TextInput style={styles.textInput} placeholder='jurandir.pereira@hotmail.com'></TextInput>
+      <Text style={styles.text}>Senha</Text>
+      <TextInput style={styles.textInput} placeholder='********'></TextInput>
+
+      <Button /*onPress={goToHome}*/ style={styles.buttonSalvar}><Text style={styles.buttonSalvarText}>Salvar</Text></Button>
+
       <Button onPress={goToAgradecimentos}>Ir para Agradecimentos</Button>
 
       <Button onPress={goToModificarPesquisa}>Ir para ModificarPesquisa</Button>
@@ -28,6 +31,50 @@ const Login = (props) => {
   )
 
 }
+const styles = StyleSheet.create({
 
+  body: {
+    backgroundColor: '#382474',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+
+  text: {
+    color: '#fff',
+    fontFamily: 'monospace',
+    fontSize: 18,
+    textAlign: 'left',
+    marginTop: 10,
+    width: 300,
+    fontWeight: 'bold',
+  },
+
+  textInput: {
+    backgroundColor: '#fff',
+    width: 300,
+    borderRadius: 0,
+    placeholder: '#3F92C5'
+  },
+
+
+  buttonSalvar: {
+    marginTop: 35,
+    width: 300,
+    backgroundColor: 'green',
+    borderRadius: 0,
+  },
+
+  buttonSalvarText: {
+    color: '#fff',
+    fontFamily: 'monospace',
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+
+
+});
 
 export default Login;
