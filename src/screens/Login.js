b904b2de77ac71ce4e2bd1ece835a77ff2815b84
Maria Gabriella Victor <109552474+gabriellavsx@users.noreply.github.com>
2024-04-15 10:23:48 -0300

    login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -15,16 +15,21 @@ const Login = (props) => {
   return (
 
     <View style={styles.body}>
+      <Text style={styles.titulo}>Satisfying.you</Text>
+      <Image></Image>
       <Text style={styles.text}>E-mail</Text>
-      <TextInput style={styles.textInput} placeholder='jurandir.pereira@hotmail.com'></TextInput>
+      <TextInput style={styles.textInput} placeholder='jurandir.pereira@hotmail.com'  placeholderTextColor= '#3F92C5'></TextInput>
+
       <Text style={styles.text}>Senha</Text>
-      <TextInput style={styles.textInput} placeholder='********'></TextInput>
+      <TextInput style={styles.textInput} placeholder='********'  placeholderTextColor='#3F92C5'></TextInput>
+      <Text style={{color:'#FD7979',textAlign:'left'}}>E-mail e/ou senhas inválidos</Text>
 
-      <Button /*onPress={goToHome}*/ style={styles.buttonSalvar}><Text style={styles.buttonSalvarText}>Salvar</Text></Button>
+      <Button /*onPress={goToHome}*/ style={styles.buttonEntrar}><Text style={styles.buttonText}>Entrar</Text></Button>
 
-      <Button onPress={goToAgradecimentos}>Ir para Agradecimentos</Button>
+      <Button /*onPress={goToNovaConta}*/ style={styles.buttonCriar}><Text style={styles.buttonText}>Criar minha conta</Text></Button>
 
-      <Button onPress={goToModificarPesquisa}>Ir para ModificarPesquisa</Button>
+      <Button /*onPress={goToRecuperarSenha}*/ style={styles.buttonEsqueci}><Text style={styles.buttonText}>Esqueci minha senha</Text></Button>
+
 
     </View>
 
@@ -34,16 +39,21 @@ const Login = (props) => {
 const styles = StyleSheet.create({
 
   body: {
-    backgroundColor: '#382474',
+    backgroundColor:'#372775',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     padding: 20,
   },
-
+  titulo:{
+    fontSize:30,
+    color:'#FFFF',
+    textAlign:'center',
+    fontFamily:'Averia Libre'
+  },
   text: {
     color: '#fff',
-    fontFamily: 'monospace',
+    fontFamily: 'Averia Libre',
     fontSize: 18,
     textAlign: 'left',
     marginTop: 10,
@@ -54,27 +64,38 @@ const styles = StyleSheet.create({
   textInput: {
     backgroundColor: '#fff',
     width: 300,
+    height:51,
     borderRadius: 0,
-    placeholder: '#3F92C5'
+    fontFamily:'Averia Libre'
   },
 
-
-  buttonSalvar: {
-    marginTop: 35,
+  buttonEntrar: {
+    marginTop: 30,
+    marginBottom:30,
     width: 300,
-    backgroundColor: 'green',
+    backgroundColor: '#37BD6D',
     borderRadius: 0,
   },
-
-  buttonSalvarText: {
+  buttonCriar: {
+    marginTop: 10,
+    width: 300,
+    backgroundColor: '#419ED7',
+    borderRadius: 0,
+  },
+  buttonEsqueci: {
+    marginTop: 15,
+    width: 300,
+    backgroundColor: '#B0CCDE',
+    borderRadius: 0,
+  },
+  buttonText: {
     color: '#fff',
-    fontFamily: 'monospace',
-    fontSize: 18,
+    fontFamily: 'Averia Libre',
+    fontSize: 15,
     textAlign: 'center',
     fontWeight: 'bold',
   },
-
-
+ 
 });
 
 export default Login;
