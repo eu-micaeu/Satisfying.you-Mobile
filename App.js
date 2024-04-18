7f04680ce85676d3a296de3f47ac3afa9637506c
carlosyamanaka <109562378+carlosyamanaka@users.noreply.github.com>
2024-04-17 22:54:16 -0300

    RecuperarSenha front feito

--- App.js
+++ App.js
@@ -6,30 +6,40 @@ import ModificarPesquisa from "./src/screens/ModificarPesquisa";
 import Home from "./src/screens/Home";
 import Drawer from "./src/screens/Drawer";
 import AcoesPesquisa from "./src/screens/AcoesPesquisa";
-import NovaConta from "./src/screens/NovaConta"
+import NovaConta from "./src/screens/NovaConta";
+import RecuperarSenha from "./src/screens/RecuperarSenha";
 
 const Stack = createStackNavigator();
 
 const App = () => {
 
     return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login"
-                                screenOptions={{ 
-                                headerTintColor: '#FFFFFF', 
-                                headerStyle: { backgroundColor: '#2B1D62' },
-                                headerTitleStyle: { fontFamily:'AveriaLibre-Bold'}
-                }}>
-                <Stack.Screen name="Login" component={Login}/>
-                <Stack.Screen name="Agradecimentos" component={Agradecimentos}/>
-                <Stack.Screen name="ModificarPesquisa" component={ModificarPesquisa}/>
-                <Stack.Screen name="Home" component={Home}/>
-                <Stack.Screen name="Drawer" component={Drawer} options={{headerShown: false}}/>
-                <Stack.Screen name="AcoesPesquisa" component={AcoesPesquisa}/>
-                <Stack.Screen name='NovaConta' component={NovaConta}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="RecuperarSenha"
+          screenOptions={{
+            headerTintColor: '#FFFFFF',
+            headerStyle: {backgroundColor: '#2B1D62'},
+            headerTitleStyle: {fontFamily: 'AveriaLibre-Bold'},
+          }}>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Agradecimentos" component={Agradecimentos} />
+          <Stack.Screen
+            name="ModificarPesquisa"
+            component={ModificarPesquisa}
+          />
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen
+            name="Drawer"
+            component={Drawer}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen name="AcoesPesquisa" component={AcoesPesquisa} />
+          <Stack.Screen name="NovaConta" component={NovaConta} />
+          <Stack.Screen name="RecuperarSenha" component={RecuperarSenha} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
 }
 
 export default App;
---
+++ src/screens/RecuperarSenha.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+import { Button } from 'react-native-paper';
+
+const RecuperarSenha = (props) => {
+
+    return (
+      <View style={styles.body}>
+        <Text style={styles.text}>E-mail</Text>
+        <TextInput
+            style={styles.textInput}
+            placeholder="jurandir.pereira@hotmail.com"
+            placeholderTextColor='#419ED7'
+            ></TextInput>
+
+        <Button /*onPress={}*/ style={styles.buttonEntrar}>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </Button>
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: '#372775',
+    flex: 1,
+    paddingTop: 150,
+    paddingLeft: 50,
+    gap: 7,
+  },
+  text: {
+    color: '#fff',
+    fontFamily: 'monospace',
+    fontSize: 18,
+    textAlign: 'left',
+    marginTop: 10,
+    width: 300,
+    fontFamily: 'AveriaLibre-Regular',
+  },
+  textInput: {
+    backgroundColor: '#fff',
+    width: 300,
+    borderRadius: 0,
+    fontFamily: 'AveriaLibre-Regular',
+  },
+  buttonEntrar: {
+    marginTop: 30,
+    marginBottom: 30,
+    width: 300,
+    backgroundColor: '#37BD6D',
+    borderRadius: 0,
+  },
+  buttonText: {
+    color: '#fff',
+    fontFamily: 'AveriaLibre-Regular',
+    fontSize: 15,
+    textAlign: 'center',
+  },
+});
+
+export default RecuperarSenha;
