9fb44ff22059a24e656d18de810b20aeeecd1f67
Monegatto <jp.monegatto@protonmail.com>
2024-04-18 20:34:24 -0300

    Conclusão da tela Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,23 +1,44 @@
 import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
-import { Button, TextInput } from 'react-native-paper';
+import { TextInput, Card } from 'react-native-paper';
 
 const Home = (props) => {
 
   goToNovaPesquisa = () => {
     props.navigation.navigate('NovaPesquisa');
   }
+  goToAcoesPesquisa = () => {
+    props.navigation.navigate('AcoesPesquisa')
+  }
+
+  const cards = [
+    {capa: require("../images/secomp.png"), nome: "SECOMP 2023", data: "10/10/2023"},
+    {capa: require("../images/ubuntu.png"), nome: "UBUNTU 2022", data: "05/06/2022"}
+  ]
 
   return (
-    <>
-
-      <View style={styles.body}>
-        <TextInput style={styles.textInput} placeholder='Insira o termo da busca...'></TextInput>
-        <Pressable onPress={goToNovaPesquisa} style={styles.buttonNovaPesquisa}>
-          <Text style={styles.buttonNovaPesquisaText}>Nova Pesquisa</Text>
-        </Pressable>
+    <View style={styles.body}>
+      <View style={styles.viewTextInput}>
+        <Image source={require('../images/pesquisa.png')}/>
+        <TextInput style={styles.textInput} placeholder='Insira o termo da busca...'/>
       </View>
-
-    </>
+      <Pressable onPress={goToAcoesPesquisa}>
+        {cards.map((card, index) => (
+          <Pressable key={index} onPress={goToAcoesPesquisa}>
+            <Card style={styles.card}>
+              <Card.Cover source={card.capa} style={styles.cardImage} />
+              <Card.Content>
+                <Text style={styles.title}>{card.nome}</Text>
+                <Text style={styles.subtitle}>{card.data}</Text>
+              </Card.Content>
+            </Card>
+          </Pressable>
+        ))}
+      </Pressable>
+      <Pressable onPress={goToNovaPesquisa} style={styles.buttonNovaPesquisa}>
+          <Text style={styles.buttonNovaPesquisaText}>Nova Pesquisa</Text>
+      </Pressable>
+  </View>
+      
   )
 }
 
@@ -29,37 +50,47 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-
-  text: {
-    color: '#fff',
-    fontFamily: 'monospace',
-    fontSize: 18,
-    textAlign: 'left',
-    marginTop: 10,
-    width: 300,
-    fontFamily: 'AveriaLibre-Regular'
+  viewTextInput: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
   },
-
   textInput: {
     backgroundColor: '#fff',
     width: 300,
     borderRadius: 0,
     fontFamily: 'AveriaLibre-Regular'
   },
-
   buttonNovaPesquisa: {
     width: 300,
     height: 50,
     backgroundColor: '#37BD6D',
     marginTop: 20
   },
-
   buttonNovaPesquisaText: {
     color: '#FFFFFF',
     textAlign: 'center',
     paddingTop: 10,
     fontFamily: 'AveriaLibre-Regular',
     fontSize: 25
+  },
+  card: {
+    width: 200,
+    alignItems: 'center',
+    margin: 15,
+  },
+  cardImage: {
+    width: 150,
+    height: 150
+  },
+  title: {
+    color: 'blue',
+    fontSize: 20,
+    fontFamily:'AveriaLibre-Regular',
+  },
+  subtitle: {
+    fontSize: 15,
+    fontFamily: 'AveriaLibre-Regular'
   }
 
 });
