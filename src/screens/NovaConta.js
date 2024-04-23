43fbdbfd7fc85e6acd0715abbac891a3be91fdb5
Daniela Alves <102709843+Daniela-Alves2004@users.noreply.github.com>
2024-04-23 14:25:46 -0300

    refactor: corrige verificação em NovaConta.js

--- src/screens/NovaConta.js
+++ src/screens/NovaConta.js
@@ -2,144 +2,140 @@ import React, { useState } from "react";
 import { View, Text, TextInput, StyleSheet, Pressable } from "react-native";
 
 const NovaConta = (props) => {
-
   const [email, setEmail] = useState('');
   const [senha1, setSenha1] = useState('');
   const [senha2, setSenha2] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessageEmail, setErrorMessageEmail] = useState('');
+  const [errorMessageSenha, setErrorMessageSenha] = useState('');
 
-  const handleEmailChange = (text) => {
-
-    setEmail(text)
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    if (!re.test(String(email).toLowerCase())) {
-      setErrorMessage("E-mail inválido")
-      return
-      
+  const handleEmail = (text) => {
+    setEmail(text);
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(text).toLowerCase())) {
+      setErrorMessageEmail("E-mail inválido");
+    } else {
+      setErrorMessageEmail("");
     }
-  }
-
-  const handleSenha1Change = (text) => {
-
-    setSenha1(text);
-    setErrorMessage('');
-
   };
 
-  const handleSenha2Change = (text) => {
+  const handleSenha1 = (text) => {
+    setSenha1(text);
+    setErrorMessageSenha('');
+  };
 
+  const handleSenha2 = (text) => {
     setSenha2(text);
-    setErrorMessage('');
+    setErrorMessageSenha('');
+  };
 
+
+
+  const cadastrarConta = () => {
+    if (senha1 !== senha2) {
+      setErrorMessageSenha("As senhas não coincidem.");
+    } else if (senha1.trim() === "" || senha2.trim() === "") {
+      setErrorMessageSenha('Preencha os campos de senha');
+    }else{
+      goToLogin();
+    }
   };
 
   const goToLogin = () => {
-
-    if (senha1 !== senha2) {
-      setErrorMessage('As senhas não coincidem.');
-      return;
-    }
     props.navigation.navigate('Login');
-
   };
 
   return (
-
     <View style={styles.body}>
+      <Text style={styles.text}>E-mail</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="jurandir.pereira@hotmail.com"
+        placeholderTextColor="#3F92C5"
+        onChangeText={handleEmail}
+        keyboardType="email-address"
+      />
+      {errorMessageEmail && (
+        <Text style={styles.errorMessage}>
+          {errorMessageEmail}
+        </Text>
+      )}
 
-        <Text style={styles.text}>E-mail</Text>
+      <Text style={styles.text}>Senha</Text>
+      <TextInput
+        style={styles.textInput}
+        secureTextEntry={true}
+        placeholder="*********"
+        placeholderTextColor="#3F92C5"
+        onChangeText={handleSenha1}
+      />
 
-        <TextInput
-          style={styles.textInput}
-          placeholder="jurandir.pereira@hotmail.com"
-          placeholderTextColor="#3F92C5"
-          onChangeText={handleEmailChange}
-          keyboardType="email-address"
-        />
+      <Text style={styles.text}>Repetir senha</Text>
+      <TextInput
+        style={styles.textInput}
+        secureTextEntry={true}
+        placeholderTextColor="#3F92C5"
+        onChangeText={handleSenha2}
+      />
 
-        {errorMessage && (
-          <Text style={{ fontSize: 17, color: '#FD7979', fontFamily: 'AveriaLibre-Regular' }}>
-            {errorMessage}
-          </Text>
-        )}
+      {errorMessageSenha && (
+        <Text style={styles.errorMessage}>
+          {errorMessageSenha}
+        </Text>
+      )}
 
-        <Text style={styles.text}>Senha</Text>
-
-        <TextInput
-          style={styles.textInput}
-          secureTextEntry={true}
-          placeholder="*********"
-          placeholderTextColor="#3F92C5"
-          onChangeText={handleSenha1Change}
-        />
-
-        <Text style={styles.text}>Repetir senha</Text>
-
-        <TextInput
-          style={styles.textInput}
-          secureTextEntry={true}
-          placeholderTextColor="#3F92C5"
-          onChangeText={handleSenha2Change}
-        />
-
-        {errorMessage && (
-          <Text style={{ fontSize: 17, color: '#FD7979', fontFamily: 'AveriaLibre-Regular' }}>
-            {errorMessage}
-          </Text>
-        )}
-
-        <Pressable onPress={goToLogin} style={styles.buttonCadastrar}>
-          <Text style={styles.buttonText}>CADASTRAR</Text>
-        </Pressable>
-
+      <Pressable onPress={cadastrarConta} style={styles.buttonCadastrar}>
+        <Text style={styles.buttonText}>CADASTRAR</Text>
+      </Pressable>
     </View>
-
   );
 };
 
 const styles = StyleSheet.create({
-
   body: {
     backgroundColor: '#372775',
     flex: 1,
-    gap: 7,
     flexDirection: 'column',
     alignItems: 'center',
   },
-
   text: {
     color: '#fff',
     fontSize: 14,
     textAlign: 'left',
     width: 300,
     fontFamily: 'AveriaLibre-Bold',
+    marginTop: 5,
   },
-
   textInput: {
     backgroundColor: '#fff',
-    fontSize:20,
+    fontSize: 20,
     width: 300,
     height: 40,
     fontFamily: 'AveriaLibre-Regular',
-    color: '#3F92C5'
+    color: '#3F92C5',
+    borderRadius: 5,
+    marginTop: 2,
+    paddingHorizontal: 10,
   },
-
   buttonCadastrar: {
     width: 300,
     height: 40,
     backgroundColor: '#37BD6D',
-    textAlign: 'center',
     justifyContent: 'center',
-    marginTop: 15
+    alignItems: 'center',
+    borderRadius: 5,
+    marginTop: 20,
   },
-
   buttonText: {
     color: '#FFFFFF',
-    textAlign: 'center',
     fontFamily: 'AveriaLibre-Regular',
-    fontSize: 20
-  }
-
-})
+    fontSize: 20,
+  },
+  errorMessage: {
+    fontSize: 14,
+    color: '#FD7979',
+    fontFamily: 'AveriaLibre-Regular',
+    marginLeft: 10,
+  },
+});
 
 export default NovaConta;
