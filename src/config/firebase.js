50132400f976413fe9f4d2a8bc67d0c1b463fbc8
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2024-08-05 05:39:13 -0300

    Refactor: Add image picker functionality to NovaPesquisa screen

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4FLb2f2wcWAJ_pdl1kmFG2Hhxs9k3FHc",
+  authDomain: "satisfyingyou-mobile-cbe6a.firebaseapp.com",
+  projectId: "satisfyingyou-mobile-cbe6a",
+  storageBucket: "satisfyingyou-mobile-cbe6a.appspot.com",
+  messagingSenderId: "616417646080",
+  appId: "1:616417646080:web:85cfb6b9691923ead1eae5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
